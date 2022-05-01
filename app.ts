89fadb3504409da31a7520344aa2345d04a6eec8
Markaa10 <rahulrajdahal@gmail.com>
2022-05-01 22:17:45 +0545

    jsonlimit

--- app.ts
+++ app.ts
@@ -22,7 +22,7 @@ const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug("app");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 const loggerOptions: expressWinston.LoggerOptions = {
   transports: [new winston.transports.Console()],
